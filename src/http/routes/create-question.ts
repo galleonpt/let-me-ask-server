76e08d13b387d8a59c2ab33809e2be6cf474ce84
galleonpt <jose15.luis00@gmail.com>
2025-07-15 23:31:43 +0100

    feat: add create question & create room & get room questions

---
+++ src/http/routes/create-question.ts
@@ -0,0 +1,42 @@
+import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
+import { z } from "zod/v4";
+import { db } from "../../database/connection.ts";
+import { schema } from "../../database/schema/index.ts";
+
+export const createQuestionRoute: FastifyPluginCallbackZod = (app) => {
+    app.post(
+        "/rooms/:roomId/questions",
+        {
+            schema: {
+                body: z.object({
+                    question: z.string().min(1),
+                }),
+                params: z.object({
+                    roomId: z.string().min(1),
+                }),
+            },
+        },
+        async (request, response) => {
+            const { question } = request.body;
+            const { roomId } = request.params;
+
+            const result = await db
+                .insert(schema.questions)
+                .values({
+                    room_id: roomId,
+                    question,
+                })
+                .returning();
+
+            const insertedQuestion = result[0];
+
+            if (!insertedQuestion) {
+                throw new Error("Failed to create new room.");
+            }
+
+            return response
+                .status(201)
+                .send({ questionId: insertedQuestion.id });
+        }
+    );
+};
--- src/http/routes/get-rooms.ts
+++ src/http/routes/get-rooms.ts
@@ -1,11 +1,8 @@
-import type { FastifyInstance } from "fastify";
 import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { db } from "../../database/connection.ts";
 import { schema } from "../../database/schema/index.ts";
 
-export const getRoomsRoute: FastifyPluginCallbackZod = (
-    app: FastifyInstance
-) => {
+export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
     app.get("/rooms", async () => {
         const results = await db
             .select({
